fae6c7c380b0b0b972c4d0acfb9f038f41f9d188
Frederick Roy <froy@lnrobo.com>
2024-08-29 15:46:30 +0900

    follow guimanager/basegui refactor

--- bindings/SofaGui/src/SofaPython3/SofaGui/Binding_GUIManager.cpp
+++ bindings/SofaGui/src/SofaPython3/SofaGui/Binding_GUIManager.cpp
@@ -60,7 +60,7 @@ void moduleAddGuiManager(py::module& m)
         :param gui_name: The name of the GUI used. See Sofa.Gui.GUIManager.ListSupportedGUI(). (optional)
         :type  gui_name: str
     )doc";
-    guiManager.def_static("Init", &sofa::gui::common::GUIManager::Init, py::arg("program_name"), py::arg("gui_name") = "", initDoc);
+    guiManager.def_static("Init", &sofa::gui::common::GUIManager::Init, py::arg("program_name"), initDoc);
 
 
     /*
@@ -75,115 +75,22 @@ void moduleAddGuiManager(py::module& m)
         :type filename: str
         :return: 0 if the GUI creation succeed, 1 otherwise
     )doc";
-    guiManager.def_static("createGUI", [](sofa::simulation::Node & node, const std::string & filename) -> int {
-        return sofa::gui::common::GUIManager::createGUI(&node, filename.c_str());
+    guiManager.def_static("createGUI", [](const char* name, sofa::simulation::Node & node, const std::string & filename) -> sofa::gui::common::BaseGUI* {
+        return sofa::gui::common::GUIManager::createGUI(name,  &node, filename.c_str());
     }, createGUIDoc);
-    guiManager.def_static("createGUI", [](sofa::simulation::Node & node) -> int {
-        return sofa::gui::common::GUIManager::createGUI(&node);
+    guiManager.def_static("createGUI", [](const char* name, sofa::simulation::Node & node) -> sofa::gui::common::BaseGUI* {
+        return sofa::gui::common::GUIManager::createGUI(name, &node);
     }, createGUIDoc);
 
 
-    /*
-     * Sofa.Gui.GUIManager.closeGUI
-     */
-    const auto closeGUIDoc = R"doc(
-        Close the current GUI.
-    )doc";
-    guiManager.def_static("closeGUI", &sofa::gui::common::GUIManager::closeGUI, closeGUIDoc);
-
-
-    /*
-     * Sofa.Gui.GUIManager.SetScene
-     */
-    const auto setSceneDoc = R"doc(
-        Set the active scene.
-
-        :param node: The root node of the current scene to draw into the GUI.
-        :type node: Node&
-        :param filename: The current script filename (optional)
-        :type filename: str
-        :param temporaryFile: Whether the file is temporary (default to false)
-        :type temporaryFile: bool
-    )doc";
-    guiManager.def_static("SetScene", [](sofa::simulation::Node & node, const char * filename = nullptr, bool temporaryFile = false) {
-        sofa::gui::common::GUIManager::SetScene(&node, filename, temporaryFile);
-    }, py::arg("root"), py::arg("filename") = nullptr, py::arg("temporaryFile") = false, setSceneDoc);
-
-
-    /*
-     * Sofa.Gui.GUIManager.MainLoop
-     */
-    const auto mainLoopDoc = R"doc(
-        Main loop of the GUI.
-
-        Calling the main loop will steal the execution control of the script until the GUI is closed.
-
-        :param node: The root node of the current scene to draw into the GUI.
-        :type node: Node&
-        :param filename: The current script filename (optional)
-        :type filename: str
-        :return: 0 if the main loop succeed, 1 otherwise
-    )doc";
-    guiManager.def_static("MainLoop", [](sofa::simulation::Node & node, const std::string & filename) -> int {
-        return sofa::gui::common::GUIManager::MainLoop(&node, filename.c_str());
-    }, mainLoopDoc);
-    guiManager.def_static("MainLoop", [](sofa::simulation::Node & node) -> int {
-        return sofa::gui::common::GUIManager::MainLoop(&node);
-    }, mainLoopDoc);
-
-
-    /*
-     * Sofa.Gui.GUIManager.SetDimension
-     */
-    const auto setDimensionDoc = R"doc(
-        Set the GUI's dimensions.
-
-        This must be called after Sofa.Gui.GUIManager.createGUI
-
-        :param width:
-        :type width: int
-        :param height:
-        :type height: int
-    )doc";
-    guiManager.def_static("SetDimension", &sofa::gui::common::GUIManager::SetDimension, setDimensionDoc);
-
-    /*
-     * Sofa.Gui.GUIManager.CenterWindow
-     */
-    const auto centerWindowDoc = R"doc(
-        Center the GUI's window on screen .
-
-        This must be called after Sofa.Gui.GUIManager.createGUI
-
-    )doc";
-    guiManager.def_static("CenterWindow", &sofa::gui::common::GUIManager::CenterWindow, centerWindowDoc);
-
-    /*
-     * Sofa.Gui.GUIManager.SetFullScreen
-     */
-    const auto SetFullScreenDoc = R"doc(
-        Set the GUI in full screen mode.
-    )doc";
-    guiManager.def_static("SetFullScreen", &sofa::gui::common::GUIManager::SetFullScreen, SetFullScreenDoc);
-
-    /*
-     * Sofa.Gui.GUIManager.SaveScreenshot
-     */
-    const auto SaveScreenshotDoc = R"doc(
-        Save the current frame into a file.
-
-        :param filename: Where to save the screenshot.
-        :type filename: str
-    )doc";
-    guiManager.def_static("SaveScreenshot", &sofa::gui::common::GUIManager::SaveScreenshot, SaveScreenshotDoc);
 
     /*
      * Sofa.Gui.GUIManager.GetGUI
      */
-    const auto GetGUIDoc = R"doc(
-        Get the current GUI.
-    )doc";
-    guiManager.def_static("GetGUI", &sofa::gui::common::GUIManager::getGUI, GetGUIDoc);
+    //const auto GetGUIDoc = R"doc(
+    //    Get the current GUI.
+    //)doc";
+    //guiManager.def_static("GetGUI", &sofa::gui::common::GUIManager::getGUI, GetGUIDoc);
 }
 
 } /// namespace sofapython3
